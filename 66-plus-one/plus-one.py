94ab8eeb769c64aeafdba8b9db2ee0417c3cbdb9
Yug Kochar <yugkochar21@gmail.com>
2026-07-10 21:32:28 +0530

    Time: 0 ms (100.00%) | Memory: 19.3 MB (55.72%) - LeetSync

---
+++ 66-plus-one/plus-one.py
@@ -0,0 +1,18 @@
+class Solution:
+    def plusOne(self, digits: List[int]) -> List[int]:
+        for i in range(len(digits)-1,-1,-1):
+            if digits[i] < 9:
+                digits[i]+= 1
+                return digits
+            digits[i] = 0
+        return [1]+ digits
+
+        # string = ""
+        # for i in range(len(digits)):
+        #     string = string + str(digits[i])
+        # value = int(string)
+        # value = str(value+1)
+        # arr = []
+        # for i in range(len(value)):
+        #     arr.append(int(value[i]))
+        # return arr
