12b31680e849580dbdad0cf6f381cbe585f0a52e
Yug Kochar <yugkochar21@gmail.com>
2026-07-23 19:50:07 +0530

    Time: 816 ms (5.00%) | Memory: 0B (100.00%) - LeetSync

--- 175-combine-two-tables/combine-two-tables.sql
+++ 175-combine-two-tables/combine-two-tables.sql
@@ -1,5 +1,5 @@
 # Write your MySQL query statement below
-select firstname, lastname,city, state
+select p.firstname, p.lastname,a.city, a.state
 from Person p
 LEFT JOIN ADDRESS a
 on p.personid = a.personid;
