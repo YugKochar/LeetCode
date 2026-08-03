06cc14a1630d48f3f492bf91f10bb24a52dd9d27
Yug Kochar <yugkochar21@gmail.com>
2026-08-03 09:27:26 +0530

    Time: 0 ms (100.00%) | Memory: 19.2 MB (62.29%) - GetLeet

---
+++ Easy/Bit-Manipulation/Minimum-Bit-Flips-to-Convert-Number/Solution.py
@@ -0,0 +1,8 @@
+class Solution:
+    def minBitFlips(self, start: int, goal: int) -> int:
+        ans = start^goal
+        count = 0 
+        for i in range(31):
+            count += (ans & 1)
+            ans = ans >>1
+        return count
