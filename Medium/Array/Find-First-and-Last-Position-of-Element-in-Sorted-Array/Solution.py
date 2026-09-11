36d1f633813800acc88676379269cc331ea31e59
Yug Kochar <yugkochar21@gmail.com>
2026-09-11 22:25:16 +0530

    Time: 0 ms (100.00%) | Memory: 20.6 MB (61.23%) - GetLeet

---
+++ Medium/Array/Find-First-and-Last-Position-of-Element-in-Sorted-Array/Solution.py
@@ -0,0 +1,39 @@
+class Solution:
+    def searchRange(self, nums: List[int], target: int) -> List[int]:
+        def firstOccurance(nums,target):
+            n = len(nums)
+            low = 0
+            high = n-1
+            first = -1
+            while low<=high:
+                mid = (low+high)//2
+                if nums[mid] ==target:
+                    first = mid
+                    high = mid-1
+                elif nums[mid] > target:
+                    high = mid -1
+                else:
+                    low = mid +1
+            return first
+        def lastOccurance(nums,target):
+            n = len(nums)
+            low = 0
+            high = n-1
+            last = -1
+            while low <= high:
+                mid = (low+high)//2
+                if nums[mid] == target:
+                    last = mid
+                    low = mid+1
+                elif nums[mid] < target:
+                    low = mid +1
+                else:
+                    high = mid-1
+            return last
+        first = firstOccurance(nums,target)
+        last = lastOccurance(nums,target)
+        return[first,last]
+        
+
+            
+            
