cecfec50cbaed4322f8d423ad166a39eaf52af45
Yug Kochar <yugkochar21@gmail.com>
2026-09-14 14:41:55 +0530

    Time: 66 ms (12.03%) | Memory: 29.6 MB (67.70%) - GetLeet

---
+++ Medium/Array/Container-With-Most-Water/Solution.py
@@ -0,0 +1,16 @@
+class Solution:
+    def maxArea(self, height: List[int]) -> int:
+        n = len(height)
+        left = 0
+        right = n-1
+        maxArea= 0
+        while left < right:
+            width = right - left
+            area = min(height[left], height[right])*width
+            maxArea = max(maxArea, area)
+        
+            if height[left] <height[right]:
+                left +=1
+            else:
+                right -=1
+        return maxArea
