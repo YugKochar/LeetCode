57bf205436c65aac5d84ac39b0b0b9cf0e06b9ae
Yug Kochar <yugkochar21@gmail.com>
2026-09-15 19:36:46 +0530

    Time: 0 ms (100.00%) | Memory: 20.4 MB (43.96%) - GetLeet

--- Easy/Array/Third-Maximum-Number/Solution.py
+++ Easy/Array/Third-Maximum-Number/Solution.py
@@ -1,9 +1,8 @@
 class Solution:
     def thirdMax(self, nums: List[int]) -> int:
         nums = list(set(nums))
-        nums.sort()
-        if len(nums) >=3:
-            return nums[-3]
-            
-        else:
+        if len(nums) <=2:
             return max(nums)
+        nums.sort()
+        return nums[-3]
+        
