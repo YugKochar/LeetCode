25d2a857e84af6073f96e2a3972af7e8c45c032f
Yug Kochar <yugkochar21@gmail.com>
2026-09-17 18:25:24 +0530

    Time: 4 ms (27.94%) | Memory: 20.3 MB (53.94%) - GetLeet

---
+++ Medium/Array/Best-Time-to-Buy-and-Sell-Stock-II/Solution.py
@@ -0,0 +1,11 @@
+class Solution:
+    def maxProfit(self, prices: list[int]) -> int:
+        n = len(prices)
+        profit = 0
+        for i in range(1, n):
+            if prices[i] > prices[i-1]:
+                profit += prices[i] - prices[i-1]
+        return profit
+            
+
+            
